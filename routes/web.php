950d640ad8dd49af75dc9527e9746b2aae7d841d
dayCod <alayarahman07@gmail.com>
2023-04-24 10:25:02 +0700

    Update: Home Controller Page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => '/'], function () {
+
+    Route::controller(HomeController::class)->group(function () {
+        Route::get('/', 'page')->name('dashboard.home');
+    });
+
 });
 
 require __DIR__ . '/auth.php';
